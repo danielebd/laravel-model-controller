eeffe1ebebfcf5fe53754f99cf3f31eb1d8dd520
danielebd <danielebalebalestro@gmail.com>
2023-05-04 16:25:43 +0200

    error : Target class [PageController] does not exist.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Laravel</title>
-         @vite('resources/js/app.js')
-    </head>
-    <body>
-        <div class="container">
-            <h1>Template Classe 89</h1>
-            <img src="{{ Vite::asset('resources/img/pluto.webp') }}" alt="" srcset="">
-        </div>
 
-    </body>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    @vite('resources/js/app.js')
+</head>
+
+<body>
+    <div class="container">
+        <h1>Movie</h1>
+        <ul>
+        @foreach ($movie as $item)
+            <li></li>
+        @endforeach
+        </ul>
+    </div>
+
+</body>
+
 </html>
